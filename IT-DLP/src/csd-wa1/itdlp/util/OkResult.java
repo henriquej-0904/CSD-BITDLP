3936c629eef5111da853b967a525dd2774428b39
Henrique Campos Ferreira <hjp.ferreira@campus.fct.unl.pt>
2022-04-15 16:03:29 +0100

    Add Http Result classes

---
+++ IT-DLP/src/csd-wa1/itdlp/util/OkResult.java
@@ -0,0 +1,33 @@
+package itdlp.util;
+
+class OkResult<T> implements Result<T> {
+    final T result;
+
+    OkResult(T result) {
+        this.result = result;
+    }
+
+    public boolean isOK() {
+        return true;
+    }
+
+    public T value() {
+        return this.result;
+    }
+
+    public int error() {
+        if (this.result == null) {
+            return 204;
+        }
+        
+        return 200;
+    }
+
+    public String toString() {
+        return "(OK, " + value() + ")";
+    }
+
+    public T resultOrThrow() {
+        return this.result;
+    }
+}
