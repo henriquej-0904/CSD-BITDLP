621208d73817e18daafb259b8a1bf76daf28456b
Henrique Campos Ferreira <hjp.ferreira@campus.fct.unl.pt>
2022-04-15 12:45:42 +0100

    Update account

--- IT-DLP/src/csd-wa1/itdlp/api/Account.java
+++ IT-DLP/src/csd-wa1/itdlp/api/Account.java
@@ -1,73 +1,37 @@
 package itdlp.api;
 
-import java.nio.ByteBuffer;
-import java.nio.charset.StandardCharsets;
-import java.security.MessageDigest;
-import java.security.PublicKey;
-
-import itdlp.api.User.UserId;
-import itdlp.util.Crypto;
-
+/**
+ * Represents an Account in the system.
+ */
 public class Account {
 
     private final AccountId id;
     private final UserId owner;
 
+    /**
+     * Create an account from the specified id and owner.
+     * @param id
+     * @param owner
+     */
     public Account(AccountId id, UserId owner){
         this.owner = owner;
         this.id = id;
     }
 
+    /**
+     * Get the account id.
+     * @return The account id.
+     */
     public AccountId getId() {
         return id;
     }
 
+    /**
+     * Get the account owner.
+     * @return The owner.
+     */
     public UserId getOwner(){
         return owner;
-    }
-
-    
-    public static class AccountId{
-
-        private final String email;
-        private final PublicKey key;
-
-        public AccountId(String email, PublicKey key){
-            this.email = email;
-            this.key = key;
-        }
-
-         /**
-         * @return the id
-         */
-        public byte[] getId() {
-            // id creation
-            MessageDigest digest = Crypto.getSha256Digest();
-
-            digest.update(email.getBytes(StandardCharsets.UTF_8));
-            //TODO: VER NA SEXTA
-            //digest.update(SRN.getBytes(StandardCharsets.UTF_8));
-
-            ByteBuffer buffer = ByteBuffer.allocate(Long.BYTES);
-            buffer.putLong(System.currentTimeMillis());
-
-            byte[] aux = digest.digest(buffer.array());
-
-            byte[] id = new byte[aux.length + key.getEncoded().length];
-            System.arraycopy(aux, 0, id, 0, aux.length);
-            System.arraycopy(key.getEncoded(), 0, id, aux.length, key.getEncoded().length);
-
-            return id;
-        }
-        
-        public String getEmail() {
-            return email;
-        }
-
-        public PublicKey getKey() {
-            return key;
-        }
-
     }
 
 }
