4a063a1f30831468edd2f1f1026f3a22806757d8
Henrique Campos Ferreira <hjp.ferreira@campus.fct.unl.pt>
2022-04-17 16:53:44 +0100

    Add start impl LedgerDBinMemory

--- IT-DLP/src/csd-wa1/itdlp/data/LedgerDBinMemory.java
+++ IT-DLP/src/csd-wa1/itdlp/data/LedgerDBinMemory.java
@@ -2,6 +2,9 @@ package itdlp.data;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 import itdlp.api.Account;
 import itdlp.api.AccountId;
@@ -17,6 +20,7 @@ public class LedgerDBinMemory extends LedgerDBlayer
 
 
     private Map<AccountId, Account> accounts;
+    private ReadWriteLock lock;
 
 
     /**
@@ -35,36 +39,109 @@ public class LedgerDBinMemory extends LedgerDBlayer
     private LedgerDBinMemory()
     {
         this.accounts = new HashMap<>();
+        this.lock = new ReentrantReadWriteLock();
     }
 
     @Override
-    public Result<Account> createAccount(Account account) {
-        // TODO Auto-generated method stub
-        return null;
+    public Result<Account> createAccount(Account account)
+    {
+        try
+        {
+            // check if account already exists.
+            lock(true);
+
+            if (this.accounts.containsKey(account.getId()))
+                return accountAlreadyExistsConflict(account.getId());
+
+            // insert account
+            try 
+            {
+                unlock(true);
+                lock(false);
+
+                if (this.accounts.putIfAbsent(account.getId(), account) != null)
+                    return accountAlreadyExistsConflict(account.getId());
+
+                return Result.ok(account);
+
+            } finally
+            {
+                lock(true);
+                unlock(false);
+            }
+
+        } finally
+        {
+            unlock(true);
+        }        
     }
 
     @Override
     public Result<Account> getAccount(AccountId accountId) {
-        // TODO Auto-generated method stub
-        return null;
+        try
+        {
+            lock(true);
+
+            Account account = this.accounts.get(accountId);
+
+            if (account == null)
+                return accountNotFound(accountId);
+
+            return Result.ok(account);
+        } finally
+        {
+            unlock(true);
+        }
     }
 
     @Override
     public Result<Integer> getBalance(AccountId accountId) {
-        // TODO Auto-generated method stub
-        return null;
+        Result<Account> accountRes = getAccount(accountId);
+        if (accountRes.isOK())
+            return Result.ok(accountRes.value().getBalance());
+
+        return Result.error(accountRes.errorException());
     }
 
     @Override
     public Result<Integer> getTotalValue(AccountId[] accounts) {
-        // TODO Auto-generated method stub
-        return null;
+        try
+        {
+            lock(true);
+
+            int total = 0;
+
+            for (AccountId accountId : accounts) {
+                Account account = this.accounts.get(accountId);
+
+                if (account == null)
+                    return accountNotFound(accountId);
+
+                total += account.getBalance();
+            }
+
+            return Result.ok(total);
+        } finally
+        {
+            unlock(true);
+        }
     }
 
     @Override
     public Result<Integer> getGlobalLedgerValue() {
-        // TODO Auto-generated method stub
-        return null;
+        try
+        {
+            lock(true);
+
+            int total = this.accounts.values().stream()
+                .mapToInt(Account::getBalance)
+                .sum();
+
+            return Result.ok(total);
+        } finally
+        {
+            unlock(true);
+        }
     }
 
     @Override
@@ -77,5 +154,17 @@ public class LedgerDBinMemory extends LedgerDBlayer
     public Result<Void> sendTransaction(AccountId origin, AccountId dest, int value) {
         // TODO Auto-generated method stub
         return null;
+    }
+
+    private void lock(boolean read)
+    {
+        Lock lock = read ? this.lock.readLock() : this.lock.writeLock();
+        lock.lock();
+    }
+
+    private void unlock(boolean read)
+    {
+        Lock lock = read ? this.lock.readLock() : this.lock.writeLock();
+        lock.unlock();
     }
 }
