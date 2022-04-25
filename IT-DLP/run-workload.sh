7f5c73dd879aa05ddbb1f0f847ef852b9b9be979
Henrique Campos Ferreira <hjp.ferreira@campus.fct.unl.pt>
2022-04-25 17:56:56 +0100

    Receive server port as input

--- IT-DLP/run-workload.sh
+++ IT-DLP/run-workload.sh
@@ -2,4 +2,4 @@
 
 docker run --rm -it --network host \
 -v "$(pwd)/tls-config/truststore.pkcs12:/app/tls-config/truststore.pkcs12" \
-csd-wa1-it-dlp-55065-52464 itdlp.tp1.tests.Workload "https://localhost:8080/rest" $*
+csd-wa1-it-dlp-55065-52464 itdlp.tp1.tests.Workload "https://localhost:$1/rest" $2 $3
