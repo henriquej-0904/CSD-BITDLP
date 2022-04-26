fe49e196cb98ac8ed9bd2f7cb678b34e73ac1149
Henrique Campos Ferreira <hjp.ferreira@campus.fct.unl.pt>
2022-04-26 01:31:02 +0100

    Update scripts

--- IT-DLP/run-bft-servers.sh
+++ IT-DLP/run-bft-servers.sh
@@ -1,1 +1,3 @@
 for ((i=0; i<4; i++)); do ./run-bft.sh $i `expr 8080 + $i`; done
+
+sleep 5
--- IT-DLP/run-bft.sh
+++ IT-DLP/run-bft.sh
@@ -4,7 +4,8 @@ replicaId=$1
 
 mongo_port=`expr 27017 + $replicaId`
 
-docker run --name "replica-$replicaId" --rm -d --network host -v "$(pwd)/tls-config/replica-$replicaId:/app/tls-config/replica-$replicaId" \
+docker run --name "replica-$replicaId" --rm -d --network host \
+-v "$(pwd)/tls-config/replica-$replicaId:/app/tls-config/replica-$replicaId" \
 -v "$(pwd)/tls-config/truststore.pkcs12:/app/tls-config/truststore.pkcs12" \
 -v "$(pwd)/db-config.properties:/app/db-config.properties" \
 -e MONGO_DB_CONNECTION_STRING="mongodb://root:password@localhost:$mongo_port" \
