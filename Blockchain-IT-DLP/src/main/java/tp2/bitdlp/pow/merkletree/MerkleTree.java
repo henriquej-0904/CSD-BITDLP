64dc9b0760b11cd9404159a320ad9a8363c975cc
pvpmadeira <pedro.madeira@clearis.pt>
2022-06-26 16:58:48 +0100

    MerkleTree: getTransaction & verify tree

--- Blockchain-IT-DLP/src/main/java/tp2/bitdlp/pow/merkletree/MerkleTree.java
+++ Blockchain-IT-DLP/src/main/java/tp2/bitdlp/pow/merkletree/MerkleTree.java
@@ -1,8 +1,13 @@
 package tp2.bitdlp.pow.merkletree;
 
+import java.security.MessageDigest;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 import tp2.bitdlp.pow.transaction.LedgerTransaction;
+import tp2.bitdlp.util.Crypto;
+import tp2.bitdlp.util.Utils;
 
 public class MerkleTree
 {
@@ -13,5 +18,49 @@ public class MerkleTree
 
     // verify tree - hashes
 
+    public boolean verifyTree(){
+        Queue<Node> buff = new LinkedList<>();
+        buff.add(merkleRoot);
+        Node node;
+
+        while(!buff.isEmpty()){
+            node = buff.remove();
+
+            if(!(node instanceof LeafNode)){
+
+                if(!node.hash.equals(getChildrenHash(node))){
+                    return false;
+                }
+
+                buff.add(node.left);
+                buff.add(node.right);
+            }
+        }
+
+        return true;
+    }
+
+    public String getChildrenHash(Node node){
+        MessageDigest digest =  Crypto.getSha256Digest();
+        digest.update(Utils.fromBase64(node.left.hash + node.right.hash));
+
+        return Utils.toBase64(digest.digest());
+    }
+
     // obter transactions
+
+    public List<LedgerTransaction> getTransactions(Node node, List<LedgerTransaction> list){
+
+        if(node == null)
+            return null;
+        
+        if(node instanceof LeafNode){
+            list.add(((LeafNode) node).getTransaction());
+        }
+
+        list.addAll(getTransactions(node.right, list));
+        list.addAll(getTransactions(node.left, list));
+
+        return list;
+    }
 }
