207cec0da1911a2232d18842000d4d15aae51264
Henrique Campos Ferreira <hjp.ferreira@campus.fct.unl.pt>
2022-06-27 12:38:00 +0100

    Create block from list of transactions

--- Blockchain-IT-DLP/src/main/java/tp2/bitdlp/pow/block/BCBlock.java
+++ Blockchain-IT-DLP/src/main/java/tp2/bitdlp/pow/block/BCBlock.java
@@ -2,21 +2,42 @@ package tp2.bitdlp.pow.block;
 
 import java.util.List;
 
+import tp2.bitdlp.pow.merkletree.MerkleTree;
 import tp2.bitdlp.pow.transaction.LedgerTransaction;
+import tp2.bitdlp.util.Utils;
 
 public class BCBlock {
 
     protected BCBlockHeader header;
 
-    protected List<LedgerTransaction> transactions;
+    protected MerkleTree transactions;
 
     public BCBlock() {
     }
 
-    public BCBlock(BCBlockHeader header, List<LedgerTransaction> transactions)
+    public BCBlock(BCBlockHeader header, MerkleTree transactions)
     {
         this.header = header;
         this.transactions = transactions;
+    }
+
+    /**
+     * Create a new block with the specified list of transactions.
+     * The previousHash field must be defined before returning to miners.
+     * 
+     * @param transactions The list of transactions of the new block.
+     * 
+     * @return A new Block without the previousHash field.
+     */
+    public static BCBlock createBlock(List<LedgerTransaction> transactions)
+    {
+        if (transactions.isEmpty())
+            throw new IllegalArgumentException("List of transactions is empty.");
+
+        MerkleTree transactionsTree = new MerkleTree(transactions);
+        return new BCBlock(
+            new BCBlockHeader(null, Utils.toHex(transactionsTree.getMerkleRootHash())),
+            transactionsTree);
     }
 
     public BCBlockHeader getHeader() {
@@ -27,11 +48,11 @@ public class BCBlock {
         this.header = header;
     }
 
-    public List<LedgerTransaction> getTransactions() {
+    public MerkleTree getTransactions() {
         return transactions;
     }
 
-    public void setTransactions(List<LedgerTransaction> transactions) {
+    public void setTransactions(MerkleTree transactions) {
         this.transactions = transactions;
     }
 }
