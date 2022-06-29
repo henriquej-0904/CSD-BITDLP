24933ca96941ef562d1a7f01ee21505c5f43aec1
Henrique Campos Ferreira <hjp.ferreira@campus.fct.unl.pt>
2022-06-30 00:28:48 +0100

    delete older scripts

--- Blockchain-IT-DLP/debug-server.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-
-docker run --rm -it --network host csd-tp2-blockchain-it-dlp-55065-52464 -agentlib:jdwp=transport=dt_socket,server=y,address=5555 tp2.bitdlp.impl.srv.CentralizedServer
--- Blockchain-IT-DLP/run-centralized-server.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-docker run --rm -it -p 8080:8080 \
--v "$(pwd)/db-config.properties:/app/db-config.properties" \
-csd-tp2-blockchain-it-dlp-55065-52464 tp2.bitdlp.impl.srv.CentralizedServer
