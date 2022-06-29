a6abab0cf86b5262c0a2da6fda62bb2a1598c56f
Henrique Campos Ferreira <hjp.ferreira@campus.fct.unl.pt>
2022-06-29 23:47:54 +0100

    update run-workload bft script

--- Blockchain-IT-DLP/run-workloadBFT.sh
+++ Blockchain-IT-DLP/run-workloadBFT.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 
-# usage: <replicaId> <nUsers> <nAccounts> <fReplicas>
+# usage: <replicaId> <fReplicas>
 
 replicaId=$1
 replicaPort=`expr 8080 + $replicaId`
 
 docker run --rm -it --network host \
 -v "$(pwd)/tls-config/truststore.pkcs12:/app/tls-config/truststore.pkcs12" \
-csd-tp2-blockchain-it-dlp-55065-52464 tp2.bitdlp.tests.WorkloadBFT "https://localhost:$replicaPort/rest" "replica-$replicaId" $2 $3 $4
+csd-tp2-blockchain-it-dlp-55065-52464 tp2.bitdlp.tests.WorkloadBFT "https://localhost:$replicaPort/rest" "replica-$replicaId" 2 1000 $2
